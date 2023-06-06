6a91a9800015006fcdfaa89b3d14d1a98d22c2d5
MuhammadUsmanAslam <musmanaslam520@gmail.com>
2023-06-06 15:33:06 +0500

    Fix rubocop errors

--- item.rb
+++ item.rb
@@ -1,31 +1,27 @@
 require 'date'
 
 class Item
-    initialize(id, genre, author, source, label, publish_date, archived)
-        @id = id
-        @genre = genre
-        @author = author
-        @source = source
-        @label = label
-        # publish date formate is YYYY, MM, DD
-        @publish_date = Date.new(publish_date) 
-        @archived = archived
-    end
+  initialize(id, genre, author, source, label, publish_date, archived)
+  @id = id
+  @genre = genre
+  @author = author
+  @source = source
+  @label = label
+  # publish date formate is YYYY, MM, DD
+  @publish_date = Date.new(publish_date)
+  @archived = archived
+end
 
-    def can_be_archived?
-        days = @publish_date - DateTime.now
-        years = days / 365
-        if years > 10
-            return true
-        end
-        return false
-    end
+def can_be_archived?
+  days = @publish_date - DateTime.now
+  years = days / 365
+  return true if years > 10
 
-    def move_to_archive
-        if can_be_archived?
-            return @archived = true
-        end
-        return false
-    end
+  false
+end
 
+def move_to_archive
+  return @archived = true if can_be_archived?
+
+  false
 end
