b31455a9e5a379edf5fc614cc0c27b52372cd79b
Tracy-miranja <barazatracy44@gmail.com>
2023-06-07 20:44:11 +0300

    Add options

--- main.rb
+++ main.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 require_relative 'musicAlbum/music_album_ui'
+=======
+require_relative './app.rb'
+>>>>>>> 8c90eca (Add options)
 
 def run_option
   puts 'Welcome to our App console!'
@@ -17,6 +21,14 @@ def run_option
 end
 
 def handle_option(option, music_album_ui) # rubocop:disable Metrics/CyclomaticComplexity
+def add_game(app)
+    puts 'add multiplayer'
+    multiplayer= gets.chomp
+    puts 'add last_played_at '
+    last_played_at= gets.chomp
+end
+
+def handle_option(option, app) # rubocop:disable Metrics/CyclomaticComplexity
   case option
   when 1
     puts 'List all books'
@@ -26,10 +38,15 @@ def handle_option(option, music_album_ui) # rubocop:disable Metrics/CyclomaticCo
     puts 'List of games'
   when 4
     music_album_ui.list_genre
+    puts 'List all music albums'
+   when 3
+    app.list_games
+  when 4
+    app.list_authors
   when 5
     puts 'List all labels'
   when 6
-    puts 'List all authors'
+    app.list_authors
   when 7
     puts 'List all sources '
   when 8
@@ -37,7 +54,7 @@ def handle_option(option, music_album_ui) # rubocop:disable Metrics/CyclomaticCo
   when 9
     music_album_ui.add_music_album
   when 10
-    puts 'Add a game'
+    add_game(app)
   when 11
     puts 'Thanks You!!...'
     false
@@ -47,10 +64,11 @@ end
 def main
   music_album_ui = MusicAlbumUI.new
   status = true
+  app=App.new
   while status
     run_option
     option = gets.chomp.to_i
-    status = handle_option(option, music_album_ui)
+    handle_option(option, app)
     status = false if option == 11
   end
 end
