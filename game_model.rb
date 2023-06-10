9f1719426b19f94522de7f9306de345100a609a4
Tracy-miranja <barazatracy44@gmail.com>
2023-06-10 23:17:24 +0300

    Correct linter error

--- game_model.rb
+++ game_model.rb
@@ -9,7 +9,6 @@ module GameModel
     puts 'Enter author author_name: '
     author_name = gets.chomp
 
-
     game = Game.new(multiplayer, last_played_at, publish_date)
     @games ||= []
     @games << game
